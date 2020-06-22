a06b1383259870cb95fb94f86e6ffa8180e4aec1
naylorna <naylorna@miamioh.edu>
2020-06-22 10:11:16 -0400

    editted html and js files

--- fib.js
+++ fib.js
@@ -4,15 +4,13 @@ function fib() {
 	var index = document.getElementById("num").value;
 	var a=1, b=0, temp;
 	while (index >= 0){
+		fibList.push(b);
 		temp = a;
 		a = a + b;
 		b = temp;
 		index--;
 	}
-	fibList.push(b);
-}
-
-function print() {
 	var x = fibList.toString();
-	document.write(x);
+        document.getElementById("result").innerHTML = x;
 }
+	
